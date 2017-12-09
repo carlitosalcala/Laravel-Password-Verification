0920f9f97255bdb62e8bb615f303fa70aa56d971
Noman Ur Rehman <nomanurrehman@gmail.com>
2017-12-09 14:08:20 +0500

    Update application routes

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/profile/{user}/edit', 'ProfileController@edit')->name('profile.edit');
+Route::put('/profile/{user}', 'ProfileController@update')->name('profile.update');
