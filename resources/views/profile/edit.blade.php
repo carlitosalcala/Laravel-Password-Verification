db63024be765cc12da2daa23f4b8c5ce5bb4c692
Noman Ur Rehman <nomanurrehman@gmail.com>
2017-12-09 15:41:06 +0500

    Update profile edit view

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -2,6 +2,25 @@
 
 @section('content')
 <div class="container">
+    @if (session('info'))
+        <div class="row">
+            <div class="col-md-12">
+                <div class="alert alert-success alert-dismissible">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                    {{ session('info') }}
+                </div>
+            </div>
+        </div>        
+    @elseif (session('error'))
+        <div class="row">
+            <div class="col-md-12">
+                <div class="alert alert-danger alert-dismissible">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                    {{ session('error') }}
+                </div>
+            </div>
+        </div>        
+    @endif
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
